50f52400072304fd8319ec58d289a872f9d3031c
Henry GOMES <hgfr09-premier@yahoo.fr>
2026-04-20 11:19:55 +0200

    Ajout d'une nouvelle tâche

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,17 +2,36 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class TaskController extends AbstractController
 {
-    #[Route('/task', name: 'app_task')]
-    public function index(): Response
+    #[Route('/', name: 'task_index')]
+    public function index(Request $request, TaskRepository $repo, EntityManagerInterface $em): Response
     {
+        $task = new Task;
+        $form = $this->createForm(TaskType::class, $task);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $task->setIsDone(false);
+            $task->setCreatedAt(new \DateTimeImmutable());
+            $em->persist($task);
+            $em->flush();
+
+            return $this->redirectToRoute('task_index');
+        }
+
         return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+            'tasks' => $repo->findBy([], ['createdAt' => 'DESC']),
+            'form' => $form
         ]);
     }
 }
